c4c0cafaef5f96dce2279b2fe258fa08f3e844bf
Michael Vinogradov <Michael@Michaels-MacBook-Pro-3.local>
2023-12-05 22:20:39 +0100

    Added script to parse duck duck go

---
+++ duck_duck_go.py
@@ -0,0 +1,53 @@
+import scrapy
+import json
+import pandas as pd
+from items import ImageItem
+
+
+class MySpider(scrapy.Spider):
+    name = 'duck_duck_go'
+    allowed_domains = ['api.duckduckgo.com']
+
+    def __init__(self, input_file=None, *args, **kwargs):
+        super(MySpider, self).__init__(*args, **kwargs)
+        self.input_file = input_file
+
+    def start_requests(self):
+        if self.input_file is None:
+            raise scrapy.exceptions.CloseSpider('Input file not specified')
+
+        # Read the input file using Pandas
+        df = pd.read_csv(self.input_file, low_memory=False)
+
+        # Filter out rows that already have a description or WikiDescription
+        df_filtered = df[df["Description"].isna() & df["WikiDescription"].isna()]
+
+        for row_dict in df_filtered.to_dict(orient="records"):
+            query = row_dict['Title']
+            url = f'http://api.duckduckgo.com/?q={query}&format=json'
+            yield scrapy.Request(url, meta={'row': row_dict}, callback=self.parse)
+
+    def parse(self, response):
+        row_dict = response.meta['row']
+        try:
+            data = json.loads(response.text)
+            if not ('Abstract' in data and 'AbstractURL' in data):
+                self.retry_request(response)
+
+            if data['Abstract'] != '':
+                row_dict["WikiLink"] = data["AbstractURL"]
+                row_dict["WikiDescription"] = data["Abstract"]
+                row_dict["image_urls"] = []
+                row_dict.pop("images", None)
+                yield ImageItem(row_dict)
+
+            self.retry_request(response)
+        except json.JSONDecodeError:
+            self.retry_request(response)
+
+    def retry_request(self, response):
+        retry_count = response.meta.get('retry_count', 0)
+        if retry_count < 5:  # Set your max retry limit
+            retry_count += 1
+            yield scrapy.Request(response.url, meta={'row': response.meta['row'], 'retry_count': retry_count}, callback=self.parse, dont_filter=True)
+
--- settings.py
+++ settings.py
@@ -1,4 +1,32 @@
 ITEM_PIPELINES = {
     'scrapy.pipelines.images.ImagesPipeline': 1,
 }
+
 IMAGE_STORES = "data/img"
+
+# Retry many times since proxies often fail
+RETRY_TIMES = 10
+# Retry on most error codes since proxies fail for different reasons
+RETRY_HTTP_CODES = [500, 503, 504, 400, 403, 404, 408]
+
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy.downloadermiddlewares.retry.RetryMiddleware': 90,
+    'scrapy_proxies.RandomProxy': 100,
+    'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': 110,
+}
+
+# Proxy list containing entries like
+# http://host1:port
+# http://username:password@host2:port
+# http://host3:port
+# ...
+PROXY_LIST = 'proxy_list.txt'
+
+# Proxy mode
+# 0 = Every requests have different proxy
+# 1 = Take only one proxy from the list and assign it to every requests
+# 2 = Put a custom proxy to use in the settings
+PROXY_MODE = 0
+
+# If proxy mode is 2 uncomment this sentence :
+#CUSTOM_PROXY = "http://host1:port"
